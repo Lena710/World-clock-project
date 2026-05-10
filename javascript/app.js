659a97d0bda5b9b732cd1503be3ea210f5f80d9f
Lena710 <85823557+Lena710@users.noreply.github.com>
2026-05-09 23:21:59 -0400

    Added functionality to select element

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,44 @@
 function displayDateTime() {
 
     let stPCityElement = document.querySelector('#stP');
-    let stPDateElement = stPCityElement.querySelector('.date');
-    let stPTimeElement = stPCityElement.querySelector('.time');
-    stPDateElement.innerHTML=moment.tz('Europe/Moscow').format('MMMM Do YYYY');
-    stPTimeElement.innerHTML=moment.tz('Europe/Moscow').format('hh:mm:ss [<small>]A[</small>]');
+    if(stPCityElement ) {
+        let stPDateElement = stPCityElement.querySelector('.date');
+        let stPTimeElement = stPCityElement.querySelector('.time');
+        stPDateElement.innerHTML=moment.tz('Europe/Moscow').format('MMMM Do YYYY');
+        stPTimeElement.innerHTML=moment.tz('Europe/Moscow').format('hh:mm:ss [<small>]A[</small>]');
 
-
+    }
+    
     let honoluluCityElement = document.querySelector('#honolulu');
-    let honoluluDateElement = honoluluCityElement.querySelector('.date');
-    let honoluluTimeElement = honoluluCityElement.querySelector('.time');
-    honoluluDateElement.innerHTML=moment.tz('Pacific/Honolulu').format('MMMM Do YYYY');
-    honoluluTimeElement.innerHTML=moment.tz('Pacific/Honolulu').format('hh:mm:ss [<small>]A[</small>]');
+    if(honoluluCityElement) {
+        let honoluluDateElement = honoluluCityElement.querySelector('.date');
+        let honoluluTimeElement = honoluluCityElement.querySelector('.time');
+        honoluluDateElement.innerHTML=moment.tz('Pacific/Honolulu').format('MMMM Do YYYY');
+        honoluluTimeElement.innerHTML=moment.tz('Pacific/Honolulu').format('hh:mm:ss [<small>]A[</small>]');
+    }
+}
+
+function changeCity(event) {
+
+    let cityValue = event.target.value;
+    if (cityValue.length !==0) {
+    let citiesElement = document.querySelector('.cities');
+    
+    let city = cityValue.replace('_', ' ').split('/')[1];
+    let cityDate = moment.tz(event.target.value).format('MMMM Do YYYY');
+    let cityTime = moment.tz(event.target.value).format('hh:mm:ss [<small>]A[</small>]');
+    citiesElement.innerHTML = `<div class="city">
+        <div>
+        <h2>${city}</h2>
+        <div class="date">${cityDate}</div>
+        </div>
+        <div class="time">${cityTime}</div>
+    </div>`;
+    }
 }
 
 
 setInterval(displayDateTime, 1000);
+
+let selectCityElement = document.querySelector('#cities');
+selectCityElement.addEventListener('change', changeCity);
